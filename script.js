e779f3c317131721605f4ba85be47e0b6f34f1e1
zooch-btw <526SDiPalma@frhsd.com>
2025-03-21 10:40:20 -0400

    part 3 of tutorial done

--- script.js
+++ script.js
@@ -69,6 +69,11 @@ const wordList = [
     'roentgenium',
     'copernicium',
     'nihonium',
+    'flerovium',
+    'moscovium',
+    'livermorium',
+    'tennessine',
+    'oganesson',
 ]
 //selecting Game Variables
 let selectedWord = '';
@@ -79,6 +84,14 @@ const maxMistakes = 6;
 
 function startGame(level) {
     selectedWord = getRandomWord(level)
+
+    //Update diff display div
+    updateDifficultyDisplay(level)
+
+    //Create placeholder's for the selected word
+    displayedWord = '_'.repeat(selectedWord.length)
+
+    document.getElementById('wordDisplay').textContent = displayedWord.split('').join(' ')
 
 
     //hide diff selection and show game area & diff box
@@ -91,6 +104,7 @@ function startGame(level) {
     document.getElementById('difficultyBox').classList.add('d-block')
 
     document.getElementById('gameArea').classList.add('d-block')
+
 }
 
 function getRandomWord(level) {
@@ -101,4 +115,17 @@ function getRandomWord(level) {
     })
 
     return filteredwords[Math.floor(Math.random() * filteredwords.length)]
+}
+
+function updateDifficultyDisplay(level) {
+    let difficultyBox = document.getElementById('difficultyBox')
+
+    //Remove any previous diff classes (easy, med, hard)
+    difficultyBox.classList.remove('easy', 'medium', 'hard')
+
+    // Set text & apply class dynamically using template literals
+    difficultyBox.textContent = `Difficulty: ${level.charAt(0).toUpperCase() + level.slice(1)}`
+
+    //apply the approprite CSS style for chosen difficulty
+    difficultyBox.classList.add(level)
 }
